82f017e61164aeddacf5d893dedd7ebc0a2555cb
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-12 14:46:02 +0200

    Aggiunto array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+var sequence = [];
 
 for (i = 1; i <= 100; i++) {
     console.log(i);
@@ -14,14 +15,17 @@ for (i = 1; i <= 100; i++) {
 
     } else if (i % 3 === 0) {
         document.writeln("Fizz");
-        
+
     } else if (i % 5 === 0) {
         document.writeln("Buzz");
      
     } else {
         document.writeln(i);
     }    
+
+    sequence.push(i)
 }
 
+console.log(sequence)
 
 
