b92b0a104fb4642dc0bfd73d17a0f218f34401a7
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-12 15:51:29 +0200

    Esercizio terminato

--- script.js
+++ script.js
@@ -39,35 +39,29 @@ var x5 = [];
 var x3e5 = [];
 
 for (i = 1; i <= 100; i++) {
-    console.log(i);
 
     var multiplo3 = i % 3 === 0;
     var multiplo5 = i % 5 === 0;
     var multiplo3e5 = i % 3 === 0 && i % 5 === 0;
 
     if (i % 3 === 0 && i % 5 === 0) {
-        document.writeln("FizzBuzz");
         x3e5.push(1);
 
     } else if (i % 3 === 0) {
-        document.writeln("Fizz");
         x3.push(1);
 
     } else if (i % 5 === 0) {
-        document.writeln("Buzz");
         x5.push(1);
-
-    } else {
-        document.writeln(i);
     }
 
     sequence.push(i)
 }
 
 console.log(sequence);
-document.getElementById("nonmultipli").innerHTML = sequence.length + " numeri";
-document.getElementById("Fizz").innerHTML = (x3.length + " Fizz (multipli di 3)");
-document.getElementById("Buzz").innerHTML = (x5.length + " Buzz (multipli di 5)");
-document.getElementById("FizzBuzz").innerHTML = (x3e5.length + " FizzBuzz (multipli sia di 3 che di cinque)");
+
+document.getElementById("nonmultipli").innerHTML = sequence.length;
+document.getElementById("Fizz").innerHTML = x3.length + " Fizz (multipli di 3)";
+document.getElementById("Buzz").innerHTML = x5.length + " Buzz (multipli di 5)";
+document.getElementById("FizzBuzz").innerHTML = x3e5.length + " FizzBuzz (multipli sia di 3 che di cinque)";
 
 
